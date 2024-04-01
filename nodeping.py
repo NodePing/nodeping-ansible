3f33f7057f22200f864023ce3b8c479c119197b6
NodePing <support@nodeping.com>
2024-04-01 16:41:46 -0700

    Update copyright to 2024

--- nodeping.py
+++ nodeping.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2022, NodePing LLC <support@nodeping.com>
+# Copyright: (c) 2024, NodePing LLC <support@nodeping.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 import inspect
